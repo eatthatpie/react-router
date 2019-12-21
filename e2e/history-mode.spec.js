b8bb5bf5bd5fce6246d918dd96ba7c9fbdcde017
eatthatpie <eatthatpie.dev@gmail.com>
2019-12-21 20:10:41 +0100

    add e2e tests

--- e2e/history-mode.spec.js
+++ e2e/history-mode.spec.js
@@ -3,12 +3,13 @@ const { createBrowser } = require('./suite');
 const browserName = process.env.BROWSER;
 
 describe(`[${browserName}] history mode`, () => {
-  test(`url changes on push`, async () => {
+  // test: first route
+
+  test(`change route once`, async () => {
     const bs = await createBrowser(browserName);
 
     try {
-      await bs.get('http://localhost:3000');
-      await (await bs.$('a')).click();
+      await bs.get('http://localhost:3000/history-mode/example');
       await (await bs.$('a[href="/page2"]')).click();
 
       expect(await bs.getCurrentUrl()).toEqual('http://localhost:3000/page2');
@@ -17,17 +18,53 @@ describe(`[${browserName}] history mode`, () => {
     }
   });
 
-  test(`url changes on history back`, async () => {
+  test(`go back`, async () => {
     const bs = await createBrowser(browserName);
 
     try {
-      await bs.get('http://localhost:3000');
-      await (await bs.$('a')).click();
+      await bs.get('http://localhost:3000/history-mode/example');
       await (await bs.$('a[href="/page2"]')).click();
       await (await bs.$('a[href="/"]')).click();
       await bs.navigate().back();
 
       expect(await bs.getCurrentUrl()).toEqual('http://localhost:3000/page2');
+    } finally {
+      await bs.quit();
+    }
+  });
+
+  // test: change the same route multiple times
+
+  test(`component change on route change`, async () => {
+    const bs = await createBrowser(browserName);
+
+    try {
+      await bs.get('http://localhost:3000/history-mode/example');
+      await (await bs.$('a[href="/"]')).click();
+
+      expect(await bs.$('h1').getText()).toEqual('This is Page 1.');
+
+      await (await bs.$('a[href="/page2"]')).click();
+
+      expect(await bs.$('h1').getText()).toEqual('This is Page 2.');
+    } finally {
+      await bs.quit();
+    }
+  });
+
+  test(`component change on route change (back)`, async () => {
+    const bs = await createBrowser(browserName);
+
+    try {
+      await bs.get('http://localhost:3000/history-mode/example');
+      await (await bs.$('a[href="/"]')).click();
+      await (await bs.$('a[href="/page2"]')).click();
+
+      expect(await bs.$('h1').getText()).toEqual('This is Page 2.');
+
+      await bs.navigate().back();
+
+      expect(await bs.$('h1').getText()).toEqual('This is Page 1.');
     } finally {
       await bs.quit();
     }
